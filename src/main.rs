092e13e2bdf0e15e388c9a5660bc81ef463b07bc
Flurin Feuerstein <flufeu@gmail.com>
2022-06-01 18:13:03 +0200

    working blinky

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![deny(unsafe_code)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_main]
 #![no_std]
 
@@ -8,24 +8,51 @@ use rtic::app;
 
 #[app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [XIP_IRQ])]
 mod app {
-    use cortex_m_semihosting::{debug, hprintln};
+    use embedded_hal::digital::v2::OutputPin;
+    use embedded_hal::digital::v2::ToggleableOutputPin;
+
+    use rp2040_monotonic::*;
+    use rp_pico::hal;
+
+    type LedPin = hal::gpio::Pin<hal::gpio::pin::bank0::Gpio25, hal::gpio::PushPullOutput>;
+
+    #[monotonic(binds = TIMER_IRQ_0, default = true)]
+    type Monotonic = Rp2040Monotonic;
 
     #[shared]
     struct Shared {}
 
     #[local]
-    struct Local {}
+    struct Local {
+        led: LedPin,
+    }
 
     #[init]
     fn init(context: init::Context) -> (Shared, Local, init::Monotonics) {
+        let mut resets = context.device.RESETS;
+        let sio = hal::Sio::new(context.device.SIO);
+        let pins = rp_pico::Pins::new(
+            context.device.IO_BANK0,
+            context.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut resets,
+        );
 
-        let _core = context.core;
-        let _device = context.device;
+        let mut led = pins.led.into_push_pull_output();
+        led.set_low().unwrap();
 
-        hprintln!("init");
+        toggle_led::spawn().ok();
 
-        debug::exit(debug::EXIT_SUCCESS);
+        (
+            Shared {},
+            Local { led },
+            init::Monotonics(Rp2040Monotonic::new(context.device.TIMER)),
+        )
+    }
 
-        (Shared {}, Local {}, init::Monotonics())
+    #[task(priority = 1, local = [led])]
+    fn toggle_led(cx: toggle_led::Context) {
+        toggle_led::spawn_after(1000.millis()).ok();
+        cx.local.led.toggle().ok();
     }
 }
